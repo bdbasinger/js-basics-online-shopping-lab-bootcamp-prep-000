8bf5fba8639a1599ec85895604776a5dd1b4df7f
Brennan B <bdbasinger777@gmail.com>
2018-07-01 06:52:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,12 +54,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === undefined) {
-    console.log("Sorry, we don't have a credit card on file for you.")
+  if (cardNumber === undefined){
+    console.log("We don't have a credit card on file for you to place your order.")
   } else {
-    var t = total()
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
     cart = []
-    return `Your total cost is $${t}, which will be charged to the card ${cardNumber}.`
+    return cart
   }
 }
 
