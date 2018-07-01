b1bbc105d21a7818d1eac602b8fb28af479a1b93
Brennan B <bdbasinger777@gmail.com>
2018-07-01 06:54:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
-    console.log("Sorry, we don't have a credit card on file for you.")
+    return "Sorry, we don't have a credit card on file for you."
   } else {
     var t = total()
     cart = []
