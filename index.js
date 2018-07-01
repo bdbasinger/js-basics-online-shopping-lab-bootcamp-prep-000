b174e359d1b3da464f59b771cc88e8d1ff60015d
Brennan B <bdbasinger777@gmail.com>
2018-07-01 06:50:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === 'undefined') {
+  if (cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
     var t = total()
